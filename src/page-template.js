1940c9494102a8f6cd7194ce6351ca0132d716e3
Riheel Hamoande <riheel83@gmail.com>
2021-04-11 23:07:55 -0500

    add email and github redirect and style the page

--- src/page-template.js
+++ src/page-template.js
@@ -12,19 +12,21 @@ function generateHTML(teamArray) {
 
     function MangerCard(teamArray) { 
         return `
-            <div class="col s4 m4">
-                <div class="card blue-grey darken-1">
-                    <div class="card-content white-text">
-                        <span class="card-title">${teamArray[0].name}</span>
-                        <p> Role: Manager </p>
-                        <p>ID : ${teamArray[0].id}</p>
-                        <p>Email: ${teamArray[0].email}</p>
-                        <p>Number: ${teamArray[0].officeNumber}</p>       
-                    </div>
-                </div>
+            <div class="col s4 m4 push-m4 s4">
+                <div class="card grey lighten-4" style="padding: 1rem;">
+                    <ul class="collection with-header ">
+                        <li class="collection-header teal white-text"><h4>${teamArray[0].name}</h4></li>
+                        <li class="collection-item"><strong>Role: </strong>${teamArray[0].role}</li>
+                        <li class="collection-item"><strong>ID: </strong>${teamArray[0].id}</li>
+                        <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[0].email}">${teamArray[0].email}</a></li>
+                        <li class="collection-item"><strong>Number: </strong>${teamArray[0].officeNumber}</li>
+                    </ul> 
+                </div>    
             </div>`  
         }
         
+
+
         function teamCard(teamArray) {
             let card=''; 
                 for( let i = 1; i < teamArray.length ; i++) {
@@ -32,31 +34,31 @@ function generateHTML(teamArray) {
                     if(teamArray[i].role == "Engineer") {
                 card += `
                 <div class="col s4 m4">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <span class="card-title">${teamArray[i].name}</span>
-                            <p> Role: ${teamArray[i].role}  </p>
-                            <p>ID : ${teamArray[i].id}</p>
-                            <p>Email: ${teamArray[i].email}</p>
-                            <p>Github: ${teamArray[i].github}</p>
-                            
-                        </div>
-                    </div>
-                </div>`
+                    <div class="card grey lighten-4" style="padding: 1rem;">
+                        <ul class="collection with-header ">
+                            <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[i].name}</h4></li>
+                            <li class="collection-item"><strong>Role: </strong>${teamArray[i].role}</li>
+                            <li class="collection-item"><strong>ID: </strong>${teamArray[i].id}</li>
+                            <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                            <li class="collection-item"><strong>Github: </strong><a href="https://github.com/${teamArray[i].github}" target="_blank">${teamArray[i].github}</a></li>
+                        </ul> 
+                    </div>    
+                </div>
+                `
                     }  else  {
-                card += `
+                card += `                
                 <div class="col s4 m4">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <span class="card-title">${teamArray[i].name}</span>
-                            <p> Role: ${teamArray[i].role}   </p>
-                            <p>ID : ${teamArray[i].id}</p>
-                            <p>Email: ${teamArray[i].email}</p>
-                            <p>School: ${teamArray[i].school}</p>
-                            
-                        </div>
-                    </div>
-                </div>`
+                    <div class="card grey lighten-4" style="padding: 1rem;">
+                        <ul class="collection with-header ">
+                            <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[i].name}</h4></li>
+                            <li class="collection-item"><strong>Role: </strong>${teamArray[i].role}</li>
+                            <li class="collection-item"><strong>ID: </strong>${teamArray[i].id}</li>
+                            <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                            <li class="collection-item"><strong>School: </strong>${teamArray[i].school}</li>
+                        </ul> 
+                    </div>    
+                </div>    
+                `
                                 }         
                 }
             return card
@@ -71,12 +73,12 @@ function generateHTML(teamArray) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>Team Profile</title>
 </head>
-<body>
+<body style="background-color: #dee6e9">
 
 <header>
-<nav>
-  <div class="nav-wrapper blue-grey darken-3 z-depth-3">
-    <a href="#" class="brand-logo">My Team Profile</a>
+<nav style="margin-bottom: 2rem;">
+  <div class="nav-wrapper teal darken-1 z-depth-3">
+    <a href="#" class="brand-logo center">T E A M Memebers</a>
   </div>
 </nav>
 </header>
