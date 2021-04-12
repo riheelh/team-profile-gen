46c39aa74b878b61f6418adcb10e98032f4ba3a2
Riheel Hamoande <riheel83@gmail.com>
2021-04-12 00:04:12 -0500

    add icons

--- src/page-template.js
+++ src/page-template.js
@@ -1,21 +1,14 @@
 const fs = require('fs');
 
-
- 
- 
-
-//function with HTML body
+//function to generate HTML page
 function generateHTML(teamArray) {
-
- 
-
-
+    //function to create manager card
     function MangerCard(teamArray) { 
         return `
             <div class="col s4 m4 push-m4 s4">
                 <div class="card grey lighten-4" style="padding: 1rem;">
                     <ul class="collection with-header ">
-                        <li class="collection-header teal white-text"><h4>${teamArray[0].name}</h4></li>
+                        <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[0].name} <i class="fab fa-jenkins" style="margin-right: 1rem; margin-left: 1rem;"></i></h4></li>
                         <li class="collection-item"><strong>Role: </strong>${teamArray[0].role}</li>
                         <li class="collection-item"><strong>ID: </strong>${teamArray[0].id}</li>
                         <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[0].email}">${teamArray[0].email}</a></li>
@@ -24,46 +17,44 @@ function generateHTML(teamArray) {
                 </div>    
             </div>`  
         }
-        
-
-
-        function teamCard(teamArray) {
-            let card=''; 
-                for( let i = 1; i < teamArray.length ; i++) {
-                    // console.log(teamArray[i])
-                    if(teamArray[i].role == "Engineer") {
-                card += `
-                <div class="col s4 m4">
-                    <div class="card grey lighten-4" style="padding: 1rem;">
-                        <ul class="collection with-header ">
-                            <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[i].name}</h4></li>
-                            <li class="collection-item"><strong>Role: </strong>${teamArray[i].role}</li>
-                            <li class="collection-item"><strong>ID: </strong>${teamArray[i].id}</li>
-                            <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
-                            <li class="collection-item"><strong>Github: </strong><a href="https://github.com/${teamArray[i].github}" target="_blank">${teamArray[i].github}</a></li>
-                        </ul> 
-                    </div>    
-                </div>
-                `
-                    }  else  {
-                card += `                
-                <div class="col s4 m4">
-                    <div class="card grey lighten-4" style="padding: 1rem;">
-                        <ul class="collection with-header ">
-                            <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[i].name}</h4></li>
-                            <li class="collection-item"><strong>Role: </strong>${teamArray[i].role}</li>
-                            <li class="collection-item"><strong>ID: </strong>${teamArray[i].id}</li>
-                            <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
-                            <li class="collection-item"><strong>School: </strong>${teamArray[i].school}</li>
-                        </ul> 
-                    </div>    
+     //function to create team members cards   
+    function teamCard(teamArray) {
+        let card=''; 
+            for( let i = 1; i < teamArray.length ; i++) {
+                if(teamArray[i].role == "Engineer") {
+            card += `
+            <div class="col s4 m4">
+                <div class="card grey lighten-4" style="padding: 1rem;">
+                    <ul class="collection with-header ">
+                        <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[i].name} <i class="fas fa-cogs" style="margin-right: 1rem; margin-left: 1rem;"></i> </h4></li>
+                        <li class="collection-item"><strong>Role: </strong>${teamArray[i].role}</li>
+                        <li class="collection-item"><strong>ID: </strong>${teamArray[i].id}</li>
+                        <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                        <li class="collection-item"><strong>Github: </strong><a href="https://github.com/${teamArray[i].github}" target="_blank">${teamArray[i].github}</a></li>
+                    </ul> 
                 </div>    
-                `
-                                }         
-                }
-            return card
-        }
+            </div>
+            `
+                }  else  {
+            card += `                
+            <div class="col s4 m4">
+                <div class="card grey lighten-4" style="padding: 1rem;">
+                    <ul class="collection with-header ">
+                        <li class="collection-header teal lighten-1 white-text"><h4>${teamArray[i].name} <i class="fas fa-graduation-cap" style="margin-right: 1rem; margin-left: 1rem;"></i></h4></li>
+                        <li class="collection-item"><strong>Role: </strong>${teamArray[i].role}</li>
+                        <li class="collection-item"><strong>ID: </strong>${teamArray[i].id}</li>
+                        <li class="collection-item"><strong>Email: </strong><a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+                        <li class="collection-item"><strong>School: </strong>${teamArray[i].school}</li>
+                    </ul> 
+                </div>    
+            </div>    
+            `
+                            }         
+            }
+        return card
+    }
     
+    //output and write to the html file
     return fs.writeFileSync('team.html', 
 `<!DOCTYPE html>
 <html lang="en">
@@ -71,6 +62,8 @@ function generateHTML(teamArray) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
     <title>Team Profile</title>
 </head>
 <body style="background-color: #dee6e9">
@@ -78,22 +71,16 @@ function generateHTML(teamArray) {
 <header>
 <nav style="margin-bottom: 2rem;">
   <div class="nav-wrapper teal darken-1 z-depth-3">
-    <a href="#" class="brand-logo center">T E A M Memebers</a>
+    <a href="#" class="brand-logo center"> M y-T E A M</a>
   </div>
 </nav>
 </header>
 <div class="container">
         <div class="row">
-
-      
-
-        ${MangerCard(teamArray)}
-
+             ${MangerCard(teamArray)}
         </div>
         <div class="row">
-
-        ${teamCard(teamArray)}
-
+            ${teamCard(teamArray)}
         </div>
 </div>
 </body>
